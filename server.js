18d8fb84d08531ed7133c62c2429f8fef65bff4b
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2025-11-23 14:06:53 -0600

    kasdlfjaksldjf

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 const express = require("express")
 const axios = require("axios")
-const { convert } = import('convert-svg-to-png');
+const sharp = require("sharp")
 const app = express()
 const port = 3000
-
-async function convertSvgToPng(svgString) {
-  try {
-    const pngBuffer = await convert(svgString);
-    // pngBuffer now contains the PNG image data
-    // You can save it to a file or use it as needed
-    return pngBuffer;
-  } catch (error) {
-    console.error('Error converting SVG to PNG:', error);
-    throw error;
-  }
-}
 
 app.get("/:letter/:position.png", (req, res) => {
   var date = new Date(Date.now())
@@ -26,10 +14,10 @@ app.get("/:letter/:position.png", (req, res) => {
     } else if(data.solution.includes(req.params.letter)) {
       color="#b59f3b"
     }
-    convertSvgToPng(`<svg width="64" height="68" viewBox="0 0 64 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+    sharp(Buffer.from(`<svg width="64" height="68" viewBox="0 0 64 68" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect y="2" width="64" height="64" fill="${color}"/>
 <text x="32" y="50" text-anchor="middle" font-family="Arial" font-size="48" font-weight="bold" fill="white">${req.params.letter.toUpperCase()}</text>
-</svg>`).then(d=>{
+</svg>`)).png().toBuffer().then(d=>{
     res.set("Access-Control-Allow-Origin","*")
     res.set("Cross-Origin-Resource-Policy", "cross-origin")
     res.setHeader('Content-Type', 'image/png');
