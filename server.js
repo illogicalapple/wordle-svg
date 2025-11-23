a8aaf867c955c8db8a8eea38a56242cc3d41ba22
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2025-11-23 13:53:12 -0600

    asdlkfjlk

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require("express")
 const axios = require("axios")
 const app = express()
 const port = 3000
+const { convert } = require('convert-svg-to-png');
 
-app.get("/:letter/:position.svg", (req, res) => {
+async function convertSvgToPng(svgString) {
+  try {
+    const pngBuffer = await convert(svgString);
+    // pngBuffer now contains the PNG image data
+    // You can save it to a file or use it as needed
+    return pngBuffer;
+  } catch (error) {
+    console.error('Error converting SVG to PNG:', error);
+    throw error;
+  }
+}
+
+app.get("/:letter/:position.png", (req, res) => {
   var date = new Date(Date.now())
   var url = `https://www.nytimes.com/svc/wordle/v2/${date.toISOString().split('T')[0]}.json`
   axios.get(url).then(response => response.data).then(data => {
@@ -13,12 +26,15 @@ app.get("/:letter/:position.svg", (req, res) => {
     } else if(data.solution.includes(req.params.letter)) {
       color="#b59f3b"
     }
-    res.set("Access-Control-Allow-Origin","*")
-    res.set("Cross-Origin-Resource-Policy", "cross-origin")
-    res.send(`<svg width="64" height="68" viewBox="0 0 64 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+    convertSvgToPng(`<svg width="64" height="68" viewBox="0 0 64 68" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect y="2" width="64" height="64" fill="${color}"/>
 <text x="32" y="50" text-anchor="middle" font-family="Arial" font-size="48" font-weight="bold" fill="white">${req.params.letter.toUpperCase()}</text>
-</svg>`)
+</svg>`).then(d=>{
+    res.set("Access-Control-Allow-Origin","*")
+    res.set("Cross-Origin-Resource-Policy", "cross-origin")
+    res.setHeader('Content-Type', 'image/png');
+    res.send(d)
+})
 })})
 
 app.listen(port);
